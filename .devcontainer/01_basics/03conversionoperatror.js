57bf4244fb1b63c13b26b70ac5afe48d1bd4f1f2
Dinesh-1206 <kandpaldinesh668@gmail.com>
2025-12-07 17:56:49 +0000

    container config done

--- .devcontainer/01_basics/03conversionoperatror.js
+++ .devcontainer/01_basics/03conversionoperatror.js
@@ -1,5 +1,5 @@
 let score="33abc"
-console.log(typeof score)
+//console.log(typeof score)
 let valueInNumber=Number(score)
 
 //console.log(typeof valueInNumber)
@@ -12,9 +12,33 @@ let valueInNumber=Number(score)
 
 let loggedIn=1;
 let isloggedIn =Boolean(loggedIn);
-console.log(isloggedIn);
+//console.log(isloggedIn);
 
 let someNumber=33
 let stringNumber=String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+//console.log(stringNumber)
+//console.log(typeof stringNumber)
+
+// **********operations************
+let val=2
+let negval=-val
+//console.log(negval)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2/2)
+// console.log(2%2)
+// console.log(2**2)
+
+let str1="hello"
+let str2=" dinesh"
+let str3=str1+str2
+console.log(str3);
+
+console.log("2"+5)//25
+console.log(2+"5")//25
+
+let gameCounter=100
+gameCounter++
+console.log(gameCounter);//post increment
