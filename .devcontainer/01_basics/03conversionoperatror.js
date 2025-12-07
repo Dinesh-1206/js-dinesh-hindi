af0f694386373ecb0282bf67da4d2ce433752068
Dinesh-1206 <kandpaldinesh668@gmail.com>
2025-12-07 13:52:25 +0000

    container config done

---
+++ .devcontainer/01_basics/03conversionoperatror.js
@@ -0,0 +1,20 @@
+let score="33abc"
+console.log(typeof score)
+let valueInNumber=Number(score)
+
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber)
+
+//"33" => 33
+//"33abc" = NAN
+//true => 1
+//false =>0
+
+let loggedIn=1;
+let isloggedIn =Boolean(loggedIn);
+console.log(isloggedIn);
+
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
