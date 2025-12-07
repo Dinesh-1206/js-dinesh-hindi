10be3bcca18e5b2ee2ab0eb474418ed6ddb6f894
Dinesh-1206 <kandpaldinesh668@gmail.com>
2025-12-07 13:13:24 +0000

    container config done

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict";//treat all JS code as anew version
+//alert(3+3) we are using node.js ,not browser
+
+console.log(3
+    +3)//code readability should be high
+ console.log("dinesh")
+
+ let name="dinesh"//string
+ let age=18//number
+ let isLoggeeedin=true//boolean
+
+ //number>= 2 to the power 52 or 53
+ //bigint
+ //string =>""
+ //Boolean =>true/false
+ //null => standalone value
+ //undefined =>
+ //symbol =>unique
+
+ //object
+ console.log(typeof "hitesh")
+ console.log(typeof null)//type is object
+  console.log(typeof undefined)//type is undefined
