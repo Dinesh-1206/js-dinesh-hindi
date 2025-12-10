7e370e3d44c71bff3de1dd8ef39a209a776eae2d
Dinesh-1206 <kandpaldinesh668@gmail.com>
2025-12-10 07:39:46 +0000

    container config done

---
+++ .devcontainer/01_basics/06_strings.js
@@ -0,0 +1,25 @@
+let name="dinesh"
+let repoCount=50
+
+console.log(`my name oi ${name} and my recount is ${repoCount}`);
+
+const gameName=new String('dinesh-kandpal')
+console.log(gameName[0])
+console.log(gameName.__proto__);
+console.log(gameName.length)
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(3));
+console.log(gameName.indexOf('n'));
+const newGame=gameName.substring(0,4);
+console.log(newGame);
+const anotherString=gameName.slice(-6,4);
+console.log(anotherString);
+
+const newString="     dinesh     "
+console.log(newString);
+console.log(newString.trim());
+
+const url="https://dinesh%20chandra.com"
+console.log(url.replace('%20','-'));
+console.log(url.includes('dinesh'));
+console.log(gameName.split('-'));
