f129e2875a6637de672bda89cca7b81df4bc88a0
Dinesh-1206 <kandpaldinesh668@gmail.com>
2025-12-12 08:07:59 +0000

    container config done

---
+++ .devcontainer/01_basics/08_date&Time.js
@@ -0,0 +1,42 @@
+//Dates
+let newDate=new Date();
+console.log(newDate);
+console.log(newDate.toString());
+console.log(newDate.toLocaleString());
+console.log(typeof newDate);
+
+let createDate=new Date(2023,0,5,6,45)
+// console.log(createDate.toDateString);
+console.log(createDate.toLocaleString());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
