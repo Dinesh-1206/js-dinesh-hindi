04282c3c9454b1a9e410afb59355409081bbd81f
Dinesh-1206 <kandpaldinesh668@gmail.com>
2025-12-12 08:54:42 +0000

    container config done

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId=1234;
+let accountEmail="dinesh@gmail.com";//
+var accountPassword="456"///it has functional scope  
+accountCity="Jaipur"
+
+// accountId=222
+accountEmail="d@gmail.com"
+accountPassword="789"
+accountCity="benguluru"
+
+console.table([accountEmail,accountId,accountPassword,accountCity])
